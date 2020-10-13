5a3dd65158b916398e060e864867f579f3048f8e
Simon Chae <s13chae@gmail.com>
2020-10-13 16:09:00 -0600

    change reducer initial state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,23 +1,19 @@
 import { INCREMENT, DECREMENT } from "../actions/index";
 
 const initialState = {
-  testNumber: [1]
+  testNumber: 1
 };
 
 function rootReducer(state = initialState, action) {
   if (action.type === INCREMENT) {
-    console.log(`INCREMENT dispatched - Previous state: [${state.testNumber}]`)
+    console.log(`INCREMENT dispatched - Previous state: ${state.testNumber}`)
     return Object.assign({}, state, {
-      testNumber: [
-        ...state.testNumber, state.testNumber[state.testNumber.length - 1] + 1
-      ]
+      testNumber: state.testNumber + 1
     })
   } else if (action.type === DECREMENT) {
-    console.log(`DECREMENT dispatched - Previous state: [${state.testNumber}]`)
+    console.log(`DECREMENT dispatched - Previous state: ${state.testNumber}`)
     return Object.assign({}, state, {
-      testNumber: [
-        ...state.testNumber, state.testNumber[state.testNumber.length - 1] - 1
-      ]
+      testNumber: state.testNumber - 1
     })
   }
   return state;
