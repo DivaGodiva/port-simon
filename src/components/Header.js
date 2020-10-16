c257894c25c58e7fad0b9693fb42cac75ec54dfe
Simon Chae <s13chae@gmail.com>
2020-10-16 15:25:24 -0600

    header clean-up 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => (
     <div id="header">
       <img src="/assets/profile.png" className="bauble" alt="YAS" />
-      <span className="title name">Simon Chae</span>
+      <span className="title">Simon Chae</span>
       <span className="title grey">Web Developer</span>
       <a className="links" href="https://www.w3schools.com">Contact</a>
       <span className="links grey"> | </span>
