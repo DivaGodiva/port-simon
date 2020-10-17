94c2755ad5bc4b385bc04606e089594b7266e205
Simon Chae <s13chae@gmail.com>
2020-10-17 14:49:59 -0600

    about styles

--- src/components/About.js
+++ src/components/About.js
@@ -2,11 +2,15 @@ import React from "react";
 
 const About = () => (
     <div id="about">
-      <h2>About</h2>
-      <span>Hello! My name is Simon. Thank you for checking out my portfolio. I have <br />
-        been on this coding adventure for about one year now. I enjoy creating <br />
-        aesthetic web-apps, and am planning on learing mobile development soon. My <br />
-        stack includes: HTML, CSS, JS, React, Redux, Axios, Node, and MongoDB.</span>
+      <h2 className="aboutheader">About</h2>
+      <h2 className="aboutlines">Hello! My name is Simon. Thank you for checking out my portfolio. I have</h2>
+      <br />
+      <h2 className="aboutlines">been on this coding adventure for about one year now. I enjoy creating</h2>
+      <br />
+      <h2 className="aboutlines">aesthetic web-apps, and am planning on learing mobile development soon. My</h2>
+      <br />
+      <h2 className="aboutlines">stack includes: HTML, CSS, JS, React, Redux, Axios, Node, and MongoDB.</h2>
+      <hr className="landingline"/>
     </div>
 )
 
