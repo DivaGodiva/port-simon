41e6c489e7ef79a8c25c776ae6d94824af9c89ad
Simon Chae <s13chae@gmail.com>
2020-10-17 17:30:26 -0600

    WIP contact 2

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,11 +3,13 @@ import React from "react";
 const Contact = () => (
     <div id="contact">
       <h2 className="contactheading">Contact</h2>
-      <h3> Simon Chae </h3>
-      <a href="https://www.w3schools.com">s13chae@gmail.com</a>
-      <a href="https://www.w3schools.com">Linked</a>
-      <a href="https://www.w3schools.com">Github</a>
-      <a href="https://www.w3schools.com">Insta</a>
+      <h3 className="name"> Simon Chae </h3>
+      <a className="name contactlinks" href="https://www.w3schools.com">s13chae@gmail.com</a>
+      <div className="name">
+        <a className="contactlinks" href="https://www.w3schools.com">Linked</a>
+        <a className="contactlinks" href="https://www.w3schools.com">Github</a>
+        <a className="contactlinks" href="https://www.w3schools.com">Insta</a>
+      </div>
       <span id="copyright">Created by me © Godivanbuns</span>
     </div>
 )
