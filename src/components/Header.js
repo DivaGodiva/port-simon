84efcbe8f31e668d565a48927e581c5939d878d4
Simon Chae <s13chae@gmail.com>
2020-10-20 16:10:47 -0600

    clean up header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,11 @@ import React from "react";
 
 const Header = () => (
     <div className="header">
-      <img src="/assets/profile.png" className="bauble" alt="YAS" />
+      <img src="/assets/profile-buns.jpg" className="bauble" alt="YAS" />
       <span className="title">Simon Chae</span>
       <span className="title grey">Web Developer</span>
       <a className="links" href="#contact">Contact</a>
-      <span className="links grey"> | </span>
       <a className="links" href="#projects">Projects</a>
-      <span className="links grey"> | </span>
       <a className="links" href="#about">About</a>
     </div>
 )
