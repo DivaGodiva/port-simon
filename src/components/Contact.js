06eee9e8f9056dc163065ef7d999e7fe79c2ebf0
Simon Chae <s13chae@gmail.com>
2020-10-20 19:57:09 -0600

    WIP soft ui upgrade

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,9 +3,9 @@ import React from "react";
 const Contact = () => (
     <div id="contact">
       <h2 className="contactheading">Contact</h2>
-      <h3 className="name"> Simon Chae </h3>
-      <a className="name contactlinks" href="https://www.w3schools.com">s13chae@gmail.com</a>
-      <div className="name">
+      <h3 className="name nameo namae"> Simon S. Chae </h3>
+      <a className="name nameo" href="https://www.w3schools.com">s13chae@gmail.com</a>
+      <div className="name wack">
         <a className="contactlinks" href="https://www.w3schools.com" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin-square"></i></a>
         <a className="contactlinks" href="https://github.com/DivaGodiva" target="_blank" rel="noopener noreferrer"><i className="fa fa-github"></i></a>
         <a className="contactlinks" href="https://www.w3schools.com" target="_blank" rel="noopener noreferrer"><i className="fa fa-instagram"></i></a>
